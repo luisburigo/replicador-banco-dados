c74a67fc01a74df75e157ff0e8f8cbf95735b496
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2020-07-27 22:28:47 -0300

    Fix: current time um tabelalog

--- api/src/models/TabelaLog.ts
+++ api/src/models/TabelaLog.ts
@@ -19,7 +19,7 @@ class TabelaLog extends BaseEntity {
     @Column('varchar', {name: 'descricao'})
     descricao: string;
 
-    @Column('datetime', {name: 'data', default: new Date().toISOString()})
+    @Column('datetime', {name: 'data', default: () => "CURRENT_TIMESTAMP"})
     data: Date;
 
     @Column('simple-enum', {name: 'type'})
